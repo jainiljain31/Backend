c00eabb1adea650f38cf1b0fc0146218428ae086
jainiljain31 <jainiljain31@gmail.com>
2024-05-06 00:19:35 +0530

    Add comment ,like,playlist and tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema, mongo } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
